55cb5d89e54bc7abcd1e1e98773570a770c37d6c
Abdulazeez Shobajo <ashobajo@c3r5s3.42prague.com>
2024-10-08 19:34:15 +0200

    echo PWD env added

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ashobajo <ashobajo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 13:01:16 by ashobajo          #+#    #+#             */
-/*   Updated: 2024/10/08 15:34:49 by ashobajo         ###   ########.fr       */
+/*   Updated: 2024/10/08 19:24:02 by ashobajo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,6 @@ int avsh_pwd(char **args);
 int avsh_export(char **args);
 int avsh_unset(char **args);
 int avsh_env(char **args);
-int avsh_exit(char **args);
 int	avsh_launch(char **argv);
 
 char	*ft_strchr(const char *s, int c)
@@ -116,9 +115,7 @@ int	main(int argc, char **argv)
 		{
 			line = readline("avsh> ");
 			if (line == NULL)
-			{
 				break;
-			}
 			if (line[0] != '\0')
 			{
 				add_history(line);
@@ -141,9 +138,15 @@ int	main(int argc, char **argv)
 				free(line);
 				break;
 			}
-
+			if (strcmp(args[0], "cd") == 0)
+				avsh_cd(args);
+			if (strcmp(args[0], "echo") == 0)
+				avsh_echo(args);
+			if (strcmp(args[0], "pwd") == 0)
+				avsh_pwd(args);
+			if (strcmp(args[0], "env") == 0)
+				avsh_env(args);
 			// avsh_launch(args);
-
 			free(line);
 		}
 	}
@@ -151,6 +154,81 @@ int	main(int argc, char **argv)
 	{
 		printf("usage: ./minishell\n");
 	}
-
 	return (0);
+}
+
+int avsh_cd(char **args)
+{
+	if (chdir(args[1]) < 0)
+	{
+		printf("Error: invalid pathname\n");
+		return (1);
+	}
+	return (0);
+}
+
+int avsh_echo(char **args)
+{
+	char	*new_str;
+	char	*old_str;
+	int i = 1;
+	int j = 0;
+		
+	if (args[1] == NULL)
+	{
+		printf("%s\n", "   ");
+		return (0);
+	}
+	
+	old_str = args[1];
+	new_str = (char *)malloc(strlen(old_str) + 1);
+	if (new_str == NULL)
+	{
+		printf("error alocating new string");
+		return (0);
+	}
+	
+	if (old_str[0] == 34 && old_str[strlen(old_str) - 1] == 34)
+	{
+		while (old_str[i] != old_str[strlen(old_str) - 1])
+			new_str[j++] = old_str[i++];
+		new_str[j] = '\0';
+	}
+	else if (old_str[0] == 39 && old_str[strlen(old_str) - 1] == 39)
+	{
+		while (old_str[i] != old_str[strlen(old_str) - 1])
+			new_str[j++] = old_str[i++];
+		new_str[j] = '\0';
+	}
+	else
+		new_str = old_str;
+	
+	printf("%s\n", new_str);
+	
+	return (0);
+}
+
+int avsh_pwd(char **args)
+{
+	char *pwd = getenv("PWD");
+	if (pwd != NULL)
+	{
+		printf("%s\n", pwd);
+	}
+	return 0;
+}
+
+int avsh_env(char **args)
+{
+	extern char **environ;
+	char **env;
+
+	env = environ;
+
+	while (*env)
+	{
+		printf("%s\n", *env);
+		env++;
+	}
+	return 0;
 }
