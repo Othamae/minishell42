6f8924cd6ec4c01ab125ff0a177a415d4b0e671b
Othamae <vero.gconesa@gmail.com>
2024-10-12 16:06:20 +0200

    Add missing functions

--- src/builtins.c
+++ src/builtins.c
@@ -6,11 +6,46 @@
 /*   By: vconesa- <vconesa-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 15:34:32 by vconesa-          #+#    #+#             */
-/*   Updated: 2024/10/12 14:48:33 by vconesa-         ###   ########.fr       */
+/*   Updated: 2024/10/12 15:31:28 by vconesa-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
+
+// find the first occurrence of a substring within a string.
+// returns pointer to first character of the first occurrence of the substring
+char *ft_strstr(const char *haystack, const char *needle)
+{
+	if (*needle == '\0')
+		return (char *)haystack;
+	while (*haystack)
+	{
+		const char *h = haystack;
+		const char *n = needle;
+		while (*h && *n && *h == *n)
+		{
+			h++;
+			n++;
+		}
+		if (*n == '\0')
+			return (char *)haystack;
+
+		haystack++;
+	}
+	return NULL;
+}
+
+int count_char(const char *str, char c)
+{
+	int count = 0;
+	while (*str)
+	{
+		if (*str == c)
+			count++;
+		str++;
+	}
+	return count;
+}
 
 
 char	*ft_strtok(char *str, const char *delim)
@@ -213,6 +248,81 @@ int vash_pwd(void)
 	return 0;
 }
 
+int vash_export(char **args)
+{
+	extern char **environ;
+	int i = 0;
+	char *new_exp;
+	char *equal_sign;
+	char *value_part;
+	int len = ft_strlen(args[1]);
+
+	if (ft_strstr(args[1], "==") != NULL)
+	{
+		printf("Error: invalid format, '==' found\n");
+		return 1;
+	}
+	if (count_char(args[1], '\'') > 2 || count_char(args[1], '"') > 2)
+	{
+		printf("Error: invalid format, quotes appear too many times\n");
+		return 1;
+	}
+	equal_sign = strchr(args[1], '=');
+
+	if (equal_sign == NULL)
+	{
+		new_exp = malloc(len + 1);
+		if (!new_exp)
+		{
+			perror("Error: memory allocation failed");
+			return 1;
+		}
+		strcpy(new_exp, args[1]);
+
+		while (environ[i] != NULL)
+		{
+			if (ft_strncmp(environ[i], new_exp, len) == 0 && environ[i][len] == '\0')
+			{
+				return 0;
+			}
+			i++;
+		}
+
+		environ[i] = new_exp;
+		environ[i + 1] = NULL;
+
+		return 0;
+	}
+	new_exp = malloc(len + 1);
+	if (!new_exp)
+	{
+		perror("Error: memory allocation failed");
+		return 1;
+	}
+	value_part = equal_sign + 1;
+	if ((*value_part == '\'' && value_part[ft_strlen(value_part) - 1] == '\'') ||
+		(*value_part == '"' && value_part[ft_strlen(value_part) - 1] == '"'))
+	{
+		value_part[ft_strlen(value_part) - 1] = '\0';
+		value_part++;
+	}
+	strncpy(new_exp, args[1], equal_sign - args[1] + 1);
+	strcpy(new_exp + (equal_sign - args[1] + 1), value_part);
+	while (environ[i] != NULL)
+	{
+		if (strncmp(environ[i], new_exp, equal_sign - args[1]) == 0)
+		{
+			environ[i] = new_exp;
+			return 0;
+		}
+		i++;
+	}
+	environ[i] = new_exp;
+	environ[i + 1] = NULL;
+	return 0;
+}
+
+
 int vash_env(void)
 {
 	extern char **environ;
@@ -228,6 +338,40 @@ int vash_env(void)
 	return 0;
 }
 
+int vash_unset(char **args)
+{
+	extern char **environ;
+	int i, j, k;
+	int len;
+
+	if (args[1] == NULL)
+	{
+		printf("Error: expected argument to unset\n");
+		return 1;
+	}
+	k = 1;
+	while (args[k] != NULL)
+	{
+		i = 0;
+		len = ft_strlen(args[k]);
+		while (environ[i] != NULL)
+		{
+			if (ft_strncmp(environ[i], args[k], len) == 0 && environ[i][len] == '=')
+			{
+				j = i;
+				while (environ[j] != NULL)
+				{
+					environ[j] = environ[j + 1];
+					j++;
+				}
+				break;
+			}
+			i++;
+		}
+		k++;
+	}
+	return 0;
+}
 
 int do_builtins(char *buff)
 {
@@ -248,26 +392,36 @@ int do_builtins(char *buff)
 		free(line);
 		return (0);
 	}
-	if (strcmp(args[0], "exit") == 0)
+	if (ft_strncmp(args[0], "exit", 5) == 0)
 	{
 		free(line);
 		exit(0);
 	}
-	if (strcmp(args[0], "cd") == 0)
+	if (ft_strncmp(args[0], "cd",3) == 0)
 		vash_cd(args);
-	else if (strcmp(args[0], "echo") == 0)
+	else if (ft_strncmp(args[0], "echo",5) == 0)
 		vash_echo(args);
-	else if (strcmp(args[0], "pwd") == 0)
+	else if (ft_strncmp(args[0], "pwd",4) == 0)
 		//vash_pwd(args);
 		ft_pwd();
-	else if (strcmp(args[0], "env") == 0)
+	else if (ft_strncmp(args[0], "env",4) == 0)
 		vash_env();
+	
+	else if (ft_strncmp(args[0], "export",7) == 0)
+	{
+		if (args[1] == NULL)
+			vash_env();
+		else
+			vash_export(args);
+	}
+	else if (ft_strncmp(args[0], "unset",6) == 0)
+		vash_unset(args);
+	// vash_launch(args);
 	else
 	{
 		free(line);
 		return (0);
 	}
-	// vash_launch(args);
 	free(line);
 	return (1);
 }
