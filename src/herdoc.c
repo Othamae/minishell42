3afa2c0f57046230a24169a10786962833392c0e
Othamae <vero.gconesa@gmail.com>
2024-10-13 17:55:56 +0200

    Refactor

--- src/herdoc.c
+++ src/herdoc.c
@@ -6,7 +6,7 @@
 /*   By: vconesa- <vconesa-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 15:54:14 by vconesa-          #+#    #+#             */
-/*   Updated: 2024/10/13 16:17:09 by vconesa-         ###   ########.fr       */
+/*   Updated: 2024/10/13 17:52:20 by vconesa-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,6 @@ static void	handle_write_error(char *line, int fd)
 	free(line);
 	close(fd);
 	exit(EXIT_FAILURE);
-}
-
-static void	exec_herdoc_cmd(int p[2], t_herdoc *hcmd)
-{
-	close(0);
-	dup(p[0]);
-	close(p[0]);
-	runcmd(hcmd->right);
 }
 
 void	handle_herdoc(t_herdoc *hcmd)
@@ -52,7 +44,7 @@ void	handle_herdoc(t_herdoc *hcmd)
 	}
 	close(p[1]);
 	if (fork1() == 0)
-		exec_herdoc_cmd(p, hcmd);
+		exec_pipe_child(hcmd->right, 0, p[0], -1);
 	close(p[0]);
 	wait(0);
 }
