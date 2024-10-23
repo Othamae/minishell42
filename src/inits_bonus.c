aba2006d70f050e6b1e5be7978be0ac359f738ef
Othamae <vero.gconesa@gmail.com>
2024-10-23 20:17:33 +0200

    Create inits_bonus

---
+++ src/inits_bonus.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   inits_bonus.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vconesa- <vconesa-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/23 20:14:43 by vconesa-          #+#    #+#             */
+/*   Updated: 2024/10/23 20:17:13 by vconesa-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/minishell.h"
+
+t_cmd	*subshellcmd(t_cmd *subcmd)
+{
+	t_subshell	*cmd;
+
+	cmd = malloc(sizeof(*cmd));
+	ft_memset(cmd, 0, sizeof(*cmd));
+	cmd->base.type = SUBSHELL_T;
+	cmd->subcmd = subcmd;
+	return ((t_cmd *)cmd);
+}
