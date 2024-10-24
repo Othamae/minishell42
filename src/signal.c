7e84f8c9afd77c0686b1564a858c6cfd4ba13090
Othamae <vero.gconesa@gmail.com>
2024-10-24 21:00:36 +0200

    Norminette

--- src/signal.c
+++ src/signal.c
@@ -3,23 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   signal.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mac <mac@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: vconesa- <vconesa-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 23:19:05 by mac               #+#    #+#             */
-/*   Updated: 2024/10/18 08:06:46 by mac              ###   ########.fr       */
+/*   Updated: 2024/10/24 21:00:22 by vconesa-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#define _GNU_SOURCE
 #include "../include/minishell.h"
 
-void handle_sigint(int sig)
+void	handle_sigint(int sig)
 {
-	static int sigint_count = 0;
+	static int	sigint_count = 0;
 
 	if (sig == SIGINT)
 	{
 		sigint_count++;
-
 		if (sigint_count == 1)
 		{
 			printf("\n%s", PROMPT);
@@ -34,47 +34,40 @@ void handle_sigint(int sig)
 	}
 }
 
-void handle_eof(int sig)
+void	handle_eof(int sig)
 {
-	if (sig == SIGUSR2) {
+	if (sig == SIGUSR2)
 		exit(0);
-	}
 }
 
-void handle_sigquit(int sig)
+void	handle_sigquit(int sig)
 {
-	static int sigquit_count = 0;
+	static int	sigquit_count = 0;
 
 	if (sig == SIGQUIT)
 	{
 		sigquit_count++;
-
 		if (sigquit_count == 1)
 		{
 			printf("\b\b  \b\b");
 		}
 		else
-		{
-			fflush(stdout);
-			printf("\b\b  \b\b");
-		}
+			write(STDOUT_FILENO, "\b\b  \b\b", 6);
 	}
 }
 
-void handle_signals(void)
+void	handle_signals(void)
 {
-	struct sigaction sa_int;
-	struct sigaction sa_eof;
-	struct sigaction sa_quit;
+	struct sigaction	sa_int;
+	struct sigaction	sa_eof;
+	struct sigaction	sa_quit;
 
 	sa_int.sa_handler = &handle_sigint;
 	sa_eof.sa_handler = &handle_eof;
 	sa_quit.sa_handler = &handle_sigquit;
-
 	sa_int.sa_flags = SA_RESTART;
 	sa_eof.sa_flags = SA_RESTART;
 	sa_quit.sa_flags = SA_RESTART;
-
 	sigaction(SIGINT, &sa_int, NULL);
 	sigaction(SIGUSR2, &sa_eof, NULL);
 	sigaction(SIGQUIT, &sa_quit, NULL);
