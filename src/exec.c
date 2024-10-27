bea72686b2ce0629ef1557bbc3ea1508e364d9a4
Othamae <vero.gconesa@gmail.com>
2024-10-27 21:08:40 +0100

    Change handle_exec to return status

--- src/exec.c
+++ src/exec.c
@@ -6,21 +6,22 @@
 /*   By: vconesa- <vconesa-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 14:59:48 by vconesa-          #+#    #+#             */
-/*   Updated: 2024/10/27 11:02:37 by vconesa-         ###   ########.fr       */
+/*   Updated: 2024/10/27 21:06:08 by vconesa-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-void	handle_exec(t_exec *ecmd)
+int	handle_exec(t_exec *ecmd)
 {
 	extern char	**environ;
 
 	if (handle_builtins(ecmd->argv))
-		return ;
+		return (0);
 	if (ecmd->argv[0] == 0)
-		exit(1);
+		return (1);
 	execve(ecmd->argv[0], ecmd->argv, environ);
 	printf("exec %s failed\n", ecmd->argv[0]);
 	exit(1);
+	return (1);
 }
