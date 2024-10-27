4e47f2ff793e9ec3cf9c5cb1fede5b0e00c1c8cd
Othamae <vero.gconesa@gmail.com>
2024-10-27 10:58:33 +0100

    Fix handle_pipe

--- src/pipe.c
+++ src/pipe.c
@@ -6,7 +6,7 @@
 /*   By: vconesa- <vconesa-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 16:22:06 by vconesa-          #+#    #+#             */
-/*   Updated: 2024/10/22 09:06:05 by vconesa-         ###   ########.fr       */
+/*   Updated: 2024/10/27 10:56:16 by vconesa-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,8 @@ int	handle_pipe(t_pipe *pcmd)
 	int	status_left;
 	int	status_right;
 
+	status_left = 0;
+	status_right = 0;
 	if (pipe(p) < 0)
 		exit_error("pipe");
 	if (fork1() == 0)
@@ -44,5 +46,6 @@ int	handle_pipe(t_pipe *pcmd)
 	close(p[0]);
 	close(p[1]);
 	wait(&status_left);
+	wait(&status_right);
 	return (WEXITSTATUS(status_right));
 }
