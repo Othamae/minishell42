b1b340ded0fb2090783a48f1a19177d967125a4a
Othamae <vero.gconesa@gmail.com>
2024-10-27 11:05:53 +0100

    Create handle_builtins

--- src/builtins.c
+++ src/builtins.c
@@ -6,7 +6,7 @@
 /*   By: vconesa- <vconesa-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 15:34:32 by vconesa-          #+#    #+#             */
-/*   Updated: 2024/10/24 21:05:50 by vconesa-         ###   ########.fr       */
+/*   Updated: 2024/10/27 10:53:34 by vconesa-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -366,9 +366,9 @@ int do_builtins(char *buff)
 		exit(0);
 	}
 	if (ft_strncmp(args[0], "cd",3) == 0)
-		vash_cd(args);
-	else if (ft_strncmp(args[0], "echo",5) == 0)
-		vash_echo(args);
+	 	vash_cd(args);
+	// else if (ft_strncmp(args[0], "echo",5) == 0)
+	// 	vash_echo(args);
 	else if (ft_strncmp(args[0], "pwd",4) == 0)
 		//vash_pwd(args);
 		ft_pwd();
@@ -392,4 +392,42 @@ int do_builtins(char *buff)
 	}
 	free(line);
 	return (1);
+}
+
+int handle_builtins(char **args) 
+{
+    if (args[0] == NULL) {
+        return 0;
+    }
+
+    if (ft_strncmp(args[0], "exit", 5) == 0) {
+        exit(0);
+    }
+    else if (ft_strncmp(args[0], "cd", 3) == 0) {
+        vash_cd(args);
+    }
+    else if (ft_strncmp(args[0], "echo", 5) == 0) {
+        vash_echo(args);
+    }
+    else if (ft_strncmp(args[0], "pwd", 4) == 0) {
+        ft_pwd();
+    }
+    else if (ft_strncmp(args[0], "env", 4) == 0) {
+        vash_env();
+    }
+    else if (ft_strncmp(args[0], "export", 7) == 0 || ft_strncmp(args[0], "setenv", 7) == 0) {
+        if (args[1] == NULL && ft_strncmp(args[0], "setenv", 7) != 0) {
+            vash_env();
+        }
+        else {
+            vash_export(args);
+        }
+    }
+    else if (ft_strncmp(args[0], "unset", 6) == 0 || ft_strncmp(args[0], "unsetenv", 9) == 0) {
+        vash_unset(args);
+    }
+    else {
+        return 0;
+    }
+    return 1;
 }
