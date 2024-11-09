9af3b8826be9c34b94c662d901c4a52054790d82
Othamae <vero.gconesa@gmail.com>
2024-11-09 21:33:31 +0100

    Move ft_setenv to utils

--- src/builtins/export_env_utils.c
+++ src/builtins/export_env_utils.c
@@ -6,11 +6,36 @@
 /*   By: vconesa- <vconesa-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 16:04:54 by vconesa-          #+#    #+#             */
-/*   Updated: 2024/11/09 20:00:22 by vconesa-         ###   ########.fr       */
+/*   Updated: 2024/11/09 21:33:00 by vconesa-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
+
+char	**expand_environ(char **environ, int size, int *count)
+{
+	int		i;
+	char	**env_copy;
+
+	*count = 0;
+	while (environ[(*count)] != NULL)
+		(*count)++;
+	env_copy = malloc(((*count) + size + 1) * sizeof(char *));
+	if (!env_copy)
+		return (NULL);
+	while (i < (*count))
+	{
+		env_copy[i] = environ[i];
+		i++;
+	}
+	while (i < ((*count) + size))
+	{
+		env_copy[i] = NULL;
+		i++;
+	}
+	env_copy[(*count) + size] = NULL;
+	return (env_copy);
+}
 
 char	**copy_environ(char **environ, int *count)
 {
@@ -57,4 +82,51 @@ void	print_sorted_env(char **environ)
 		i++;
 	}
 	free(env_copy);
+}
+
+int	edit_variable(char *name, char *new_var, char ***env_copy)
+{
+	int	i;
+
+	i = 0;
+	while ((*env_copy)[i] != NULL)
+	{
+		if (ft_strncmp((*env_copy)[i], name, ft_strlen(name)) == 0 &&
+			(*env_copy)[i][ft_strlen(name)] == '=')
+		{
+			free((*env_copy)[i]);
+			(*env_copy)[i] = new_var;
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
+
+int	ft_setenv(char *name, char *value, char ***environ)
+{
+	char	**env_copy;
+	size_t	len;
+	int		count;
+	char	*new_var;
+
+	len = ft_strlen(name) + ft_strlen(value) + 2;
+	env_copy = expand_environ(*environ, len, &count);
+	new_var = malloc(len);
+	if (!new_var)
+		return (1);
+	ft_strlcpy(new_var, name, ft_strlen(name) + 1);
+	ft_strlcat(new_var, "=", ft_strlen(new_var) + 2);
+	ft_strlcat(new_var, value, len);
+	if (edit_variable(name, new_var, &env_copy))
+	{
+		free(*environ);
+		*environ = env_copy;
+		return (0);
+	}
+	env_copy[count] = new_var;
+	env_copy[count + 1] = NULL;
+	free(*environ);
+	*environ = env_copy;
+	return (0);
 }
