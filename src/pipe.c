5522df2f8235a14fb1e038b46ce1994be37100ae
Othamae <vero.gconesa@gmail.com>
2024-11-09 11:16:14 +0100

    Add contect to pipe functions

--- src/pipe.c
+++ src/pipe.c
@@ -6,13 +6,13 @@
 /*   By: vconesa- <vconesa-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 16:22:06 by vconesa-          #+#    #+#             */
-/*   Updated: 2024/10/27 10:56:16 by vconesa-         ###   ########.fr       */
+/*   Updated: 2024/11/08 18:13:39 by vconesa-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-void	exec_pipe_child(t_cmd *cmd, int p[2], int is_left)
+void	exec_pipe_child(t_cmd *cmd, int p[2], int is_left, t_context *context)
 {
 	if (is_left)
 	{
@@ -26,10 +26,10 @@ void	exec_pipe_child(t_cmd *cmd, int p[2], int is_left)
 	}
 	close(p[0]);
 	close(p[1]);
-	runcmd(cmd);
+	runcmd(cmd, context);
 }
 
-int	handle_pipe(t_pipe *pcmd)
+int	handle_pipe(t_pipe *pcmd, t_context *context)
 {
 	int	p[2];
 	int	status_left;
@@ -40,12 +40,14 @@ int	handle_pipe(t_pipe *pcmd)
 	if (pipe(p) < 0)
 		exit_error("pipe");
 	if (fork1() == 0)
-		exec_pipe_child(pcmd->left, p, 1);
+		exec_pipe_child(pcmd->left, p, 1, context);
 	if (fork1() == 0)
-		exec_pipe_child(pcmd->right, p, 0);
+		exec_pipe_child(pcmd->right, p, 0, context);
 	close(p[0]);
 	close(p[1]);
 	wait(&status_left);
 	wait(&status_right);
-	return (WEXITSTATUS(status_right));
+	status_left = WEXITSTATUS(status_left);
+	status_right = WEXITSTATUS(status_right);
+	return (status_right);
 }
