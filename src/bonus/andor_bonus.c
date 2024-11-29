4c82564de82c774c985a028ddd4525b6a90e8bcc
Othamae <vero.gconesa@gmail.com>
2024-11-29 17:46:03 +0100

    Fix and_or

--- src/bonus/andor_bonus.c
+++ src/bonus/andor_bonus.c
@@ -6,38 +6,39 @@
 /*   By: vconesa- <vconesa-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 13:01:50 by vconesa-          #+#    #+#             */
-/*   Updated: 2024/11/29 12:50:00 by vconesa-         ###   ########.fr       */
+/*   Updated: 2024/11/29 17:45:47 by vconesa-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-void	handle_and_or(t_clist *lcmd, t_context *context)
+static void	check_next_cmd(t_clist *lcmd, t_context *context)
 {
 	t_clist	*right;
 
+	right = (t_clist *)lcmd->right;
+	if (right)
+		runcmd(right->right, context);
+}
+
+void	handle_and_or(t_clist *lcmd, t_context *context)
+{
 	runcmd(lcmd->left, context);
 	if (lcmd->base.type == AND_T)
 	{
 		if (context->last_status == 0)
 			runcmd(lcmd->right, context);
-		else if (lcmd->right->type != EXEC_T)
-		{
-			right = (t_clist *)lcmd->right;
-			if (right)
-				runcmd(right->right, context);
-		}
+		else if (lcmd->right->type != EXEC_T && lcmd->right->type != PIPE_T
+			&& lcmd->right->type != AND_T)
+			check_next_cmd(lcmd, context);
 	}
 	else if (lcmd->base.type == OR_T)
 	{
 		if (context->last_status != 0)
 			runcmd(lcmd->right, context);
-		else if (lcmd->right->type != EXEC_T)
-		{
-			right = (t_clist *)lcmd->right;
-			if (right)
-				runcmd(right->right, context);
-		}
+		else if (lcmd->right->type != EXEC_T && lcmd->right->type != PIPE_T
+			&& lcmd->right->type != OR_T)
+			check_next_cmd(lcmd, context);
 	}
 }
 
