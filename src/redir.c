2df521178bb7bf514f0239a8677f4e0f0e6cf3c8
Othamae <vero.gconesa@gmail.com>
2024-12-01 19:35:29 +0100

    Fix signals in redir

--- src/redir.c
+++ src/redir.c
@@ -6,7 +6,7 @@
 /*   By: vconesa- <vconesa-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 15:54:14 by vconesa-          #+#    #+#             */
-/*   Updated: 2024/12/01 16:40:30 by vconesa-         ###   ########.fr       */
+/*   Updated: 2024/12/01 19:35:06 by vconesa-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,20 @@ static void	handle_write_error(char *line, int fd)
 	exit(EXIT_FAILURE);
 }
 
-static void	process_herdoc(char	*line, t_herdoc *hcmd, int p[2])
+static void	process_herdoc(char	*line, t_herdoc *hcmd, int p[2]
+	, t_context *context)
 {
+	signal(SIGINT, handle_sigint_herdoc);
 	while (1)
 	{
-		signal(SIGINT, handle_sigint_herdoc);
 		if (g_signal_received == SIGINT)
-			break ;
+			exit(context->last_status = 130);
 		line = readline("> ");
 		if (!line || g_signal_received == SIGINT)
+		{
+			context->last_status = 130;
 			break ;
+		}
 		if (ft_strncmp(line, hcmd->delim, ft_strlen(hcmd->delim)) == 0)
 		{
 			free(line);
@@ -40,6 +44,17 @@ static void	process_herdoc(char	*line, t_herdoc *hcmd, int p[2])
 			handle_write_error(line, p[1]);
 		free(line);
 	}
+	signal(SIGINT, SIG_DFL);
+}
+
+void	handle_last_status(t_context *context)
+{
+	if (WIFEXITED(context->last_status))
+		context->last_status = WEXITSTATUS(context->last_status);
+	else if (WIFSIGNALED(context->last_status))
+		context->last_status = 128 + WTERMSIG(context->last_status);
+	signal(SIGINT, handle_sigint);
+	signal(SIGQUIT, handle_sigquit);
 }
 
 void	handle_herdoc(t_herdoc *hcmd, t_context *context)
@@ -53,7 +68,7 @@ void	handle_herdoc(t_herdoc *hcmd, t_context *context)
 	if (fork1() == 0)
 	{
 		default_signals();
-		process_herdoc(line, hcmd, p);
+		process_herdoc(line, hcmd, p, context);
 		close(p[1]);
 		if (fork1() == 0)
 		{
@@ -61,14 +76,14 @@ void	handle_herdoc(t_herdoc *hcmd, t_context *context)
 			exit(context->last_status);
 		}
 		ignore_signals();
-		wait(0);
-		exit(0);
+		wait(&context->last_status);
+		handle_signals();
+		exit(WEXITSTATUS(context->last_status));
 	}
 	close(p[1]);
 	ignore_signals();
 	wait(&context->last_status);
-	signal(SIGINT, handle_sigint);
-	signal(SIGQUIT, handle_sigquit);
+	handle_last_status(context);
 }
 
 void	handle_redir(t_redir *rcmd, t_context *context)
